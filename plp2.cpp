9b3129bd1b02979491a0cc1cec980c8d9ee0d192
MaryMay1923 <104173473+MaryMay1923@users.noreply.github.com>
2022-12-06 20:24:08 -0500

    Update plp2.cpp

--- plp2.cpp
+++ plp2.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 
-//one line comment
-/*multiline
-comment*/
-
-
 int main()
 {
-
+  bool to_print = true; //initialize variable
+  cout << to_print << endl; //outputs 1
+  
+  int people = 1;
+  string hello = ""; //declaired here so its within the scope
+  
+  if (people > 1) {
+    hello = "Hello everyone!";
+  }
+  else if (people == 1) {
+    hello = "Hey!";
+  }
+  else {
+    hello = "Where is everyone?";
+  }
+  cout << hello << endl; //outputs "Hey!"
+  
+  /*if there's more than one person, hello = "Hello everyone!"
+    otherwise, hello = "Hey!"*/
+  string hello = (people >1) ? "Hello everyone!" : "Hey!";
+  cout << hello << endl; //outputs "Hey!"
   
 
 }
