f61636daf65b6d1682f83ef5459084fbe4b3d022
MaryMay1923 <104173473+MaryMay1923@users.noreply.github.com>
2022-12-06 20:43:18 -0500

    Update plp1.cpp

--- plp1.cpp
+++ plp1.cpp
@@ -4,21 +4,7 @@ using namespace std;
 /*multiline
 comment*/
 
-//type name = value;
-
 int main() {
-  int best_number = 3; //initialize variable
-  cout << best_number<<endl; //outputs 3
-  best_number = 5; //updated value
-  cout << best_number<<endl; //outputs 5
-  best_number = "a"; //doesn't work
-
-  int best_number = 3; //initialize variable
-  float worst_number = 0.33;
-  float togetherF = best_number + worst_number; //outputs 3.33
-  int togetherI = best_number + worst_number; //outputs 3
-  cout << togetherI<<endl;
-
   cout << "Hello World!"<<endl;
   return 0;
 }
